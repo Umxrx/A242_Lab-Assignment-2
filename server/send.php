033ed34c009fdd3a3febe52c2a9f4bd0852fe9bf
Umxrx <100037825+Umxrx@users.noreply.github.com>
2025-05-25 12:46:20 +0800

    Create send.php

---
+++ server/send.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $message = $_POST["message"];
+
+    $url = "https://iot-project-f46ce-default-rtdb.asia-southeast1.firebasedatabase.app/oled_text.json";
+
+    $data = json_encode($message);
+
+    $options = [
+        "http" => [
+            "header" => "Content-type: application/json\r\n",
+            "method" => "PUT", // Use PUT to replace value
+            "content" => $data
+        ]
+    ];
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === FALSE) {
+        die("Error sending to Firebase.");
+    }
+
+    header("Location: index.php");
+    exit;
+}
+?>
